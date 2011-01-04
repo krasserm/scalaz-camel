d00e77ef29d7343a8d338f0e4d32f30fd89c7fea
Martin Krasser <krasserm@googlemail.com>
2011-01-04 19:03:20 +0100

    Historical synchronous and blocking router (will be maintained for experimental purposes)

--- src/main/scala/scalaz/camel/async/Camel.scala
+++
@@ -1,9 +0,0 @@
-package scalaz.camel.async
-
-/**
- * Experimental support for asynchronous Camel processors and endpoint producers. Allows the
- * construction of non-blocking routes. Provides a DSL on top of a CPS-based implementation.
- *
- * @author Martin Krasser
- */
-object Camel extends CamelDsl
