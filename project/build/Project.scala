fe61b89a83dc0e9de3830395c239c1085d353b27
Martin Krasser <krasserm@googlemail.com>
2011-01-08 05:56:10 +0100

    Changed some dependencies from scope compile to test

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -5,13 +5,13 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
   val CamelVersion = "2.5.0"
 
   val scalazCore = "com.googlecode.scalaz" % "scalaz-core_2.8.0" % ScalazVersion % "compile"
-
   val camelCore = "org.apache.camel" % "camel-core" % CamelVersion % "compile" withSources
-  val camelJms = "org.apache.camel" % "camel-jms" % CamelVersion % "compile" withSources
-  val cameHttp = "org.apache.camel" % "camel-http" % CamelVersion % "compile" withSources
-  val camelJetty = "org.apache.camel" % "camel-jetty" % CamelVersion % "compile" withSources
-  val camelSpring = "org.apache.camel" % "camel-spring" % CamelVersion % "compile" withSources
-  val activemqCore = "org.apache.activemq" % "activemq-core" % "5.3.2" % "compile" withSources
+
+  val camelJms = "org.apache.camel" % "camel-jms" % CamelVersion % "test" withSources
+  val cameHttp = "org.apache.camel" % "camel-http" % CamelVersion % "test" withSources
+  val camelJetty = "org.apache.camel" % "camel-jetty" % CamelVersion % "test" withSources
+  val camelSpring = "org.apache.camel" % "camel-spring" % CamelVersion % "test" withSources
+  val activemqCore = "org.apache.activemq" % "activemq-core" % "5.3.2" % "test" withSources
 
   val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
   val junit = "junit" % "junit" % "4.8.2" % "test"
